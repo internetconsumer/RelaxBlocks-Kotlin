60b5d7ec700eedbedea46662eb0d693b19e4027f
milo.holthof <milo.holthof@student.kdg.be>
2022-04-05 02:52:06 +0200

    Demo Class with tests & changes to Game.kt

--- RelaxBlocks2/src/RelaxBlocks/model/Game.kt
+++ RelaxBlocks2/src/RelaxBlocks/model/Game.kt
@@ -1,11 +1,42 @@
 package RelaxBlocks.model
 
-class Game {
+import kotlin.random.Random
+
+class Game(val height: Int, val width: Int) {
+
+
+    init {
+        if (height < 0 || width < 0) {
+            throw IllegalArgumentException("ERROR. Game height or width invalid")
+        }
+    }
 
 }
 
-class Cell {
 
+class Cell(val y: Int, val x: Int) {
+    var cellValue = Random.nextInt(3)
+    var cellType: Char? = null
+
+    init {
+        if (cellValue > 3 || cellValue < 0) {
+            throw IllegalArgumentException("ERROR. CellValue Illegal")
+        }
+        setCellType(cellValue)
+    }
+
+    //    Give cell value depending on filled(0 until 2) or empty(3)
+    fun setCellType(cellValue: Int) {
+        when (cellValue) {
+            0 -> cellType = '0'
+            1 -> cellType = '+'
+            2 -> cellType = 'X'
+            3 -> cellType = '▮'
+            else -> {
+                throw IllegalArgumentException("IllegalArgument")
+            }
+        }
+    }
 }
 
 class Player(val name: String) {
--- RelaxBlocks2/src/RelaxBlocks/model/RLXDemo.kt
+++ RelaxBlocks2/src/RelaxBlocks/model/RLXDemo.kt
@@ -1,5 +1,19 @@
 package RelaxBlocks.model
 
 fun main() {
-println("Hello World")
+    val player = Player("player1")
+    val testTemplate = "%s currently has %d points."
+    println(testTemplate.format(player.name, player.score))
+    val templateTwo = "Cell %-10d X coordinate: %-8dY coordinate: %d\nCellType: %s"
+    var testingTime = arrayListOf<Cell>()
+    for (i in 0 until 10) {
+        testingTime.add(i, Cell(0, i))
+    }
+
+    for (i in 0 until 10) {
+        println(templateTwo.format(i,testingTime[i].x, testingTime[i].y, testingTime[i].cellType))
+    }
+    val cell = Cell(5, 5)  // Cell test, illegal CellValue
+    val templateThree = "X coordinate: %-8dY coordinate: %d\nCellType: %s"
+    println(templateThree.format(cell.x, cell.y, cell.cellType))
 }
